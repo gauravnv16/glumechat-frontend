a93a62f46fdaba9899a86aa2630df9c539692134
gauravnv16 <114969570+gauravnv16@users.noreply.github.com>
2022-12-20 18:49:08 +0530

    Update API.jsx

--- DataBase/API.jsx
+++ DataBase/API.jsx
@@ -1,2 +1,3 @@
-export const API = "https://glumechat-backend.vercel.app/";
+// export const API = "https://glumechat-backend.vercel.app/";
 // export const API = "http://localhost:3000/";
+export const API = "https://glumechatbackend.onrender.com/";
